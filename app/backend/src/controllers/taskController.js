c92cc59e26170e831234f128d30e4d162b00ab3a
Braien Machado de Paula <braienmp@outlook.com>
2022-05-19 09:55:38 -0300

    fix: all endpoints

--- app/backend/src/controllers/taskController.js
+++ app/backend/src/controllers/taskController.js
@@ -1,15 +1,42 @@
+const { sequelize } = require('../database/models');
 const TaskService = require('../services/taskService');
 
-const getAllTasks = async (_req, res) => {
-  try {
-    const tasks = await TaskService.getAllTasks();
+const createTask = async (req, res) => {
+  const { task, status } = req.body;
 
-    return res.status(200).json(tasks);
-  } catch (e) {
-    return res.status(500).json({ message: e.message });
-  }
+  const createdTask = await TaskService.createTask({ task, status });
+
+  res.status(201).json(createdTask);
+};
+
+const getAllTasks = async (_req, res) => {
+  const tasks = await TaskService.getAllTasks();
+
+  res.status(200).json(tasks);
+};
+
+const deleteTask = async (req, res) => {
+  const { id } = req.params;
+  await TaskService.deleteTask(id);
+
+  res.status(204).send();
+};
+
+const updateTask = async (req, res) => {
+  const { task, status } = req.body;
+  const { id } = req.params;
+
+  const updatedTask = await sequelize.transaction(async (t) => {
+    await TaskService.updateTask({ task, status }, id, t);
+    return TaskService.getTaskById(id, t);
+  });
+
+  res.status(200).json(updatedTask);
 };
 
 module.exports = {
+  createTask,
+  deleteTask,
   getAllTasks,
+  updateTask,
 };
--- app/backend/src/routes/taskRoutes.js
+++ app/backend/src/routes/taskRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
-const { getAllTasks } = require('../controllers/taskController');
+const {
+  createTask,
+  deleteTask,
+  getAllTasks,
+  updateTask,
+} = require('../controllers/taskController');
+const validateId = require('../middlewares/idValidation');
 
 const router = express.Router();
 
+router.delete('/:id', validateId, deleteTask);
 router.get('/', getAllTasks);
+router.post('/', createTask);
+router.put('/:id', validateId, updateTask);
 
 module.exports = router;

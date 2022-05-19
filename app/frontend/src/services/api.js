3169f3cb44e7fdbe8b000832f7eb9a45a40f391f
Braien Machado de Paula <braienmp@outlook.com>
2022-05-19 18:09:50 -0300

    fix: change requests

--- app/frontend/src/services/api.js
+++ app/frontend/src/services/api.js
@@ -1,10 +1,22 @@
+const axios = require('axios');
+
 const getTasks = async () => {
-  const tasks = await fetch('http://localhost:3001/task')
-    .then((response) => response.json());
+  const tasks = await axios.get('http://localhost:3001/task')
+    .then((response) => response.data);
 
   return tasks;
 };
 
+const addTask = async (description) => {
+  const result = await axios.post('http://localhost:3001/task', {
+    task: description,
+    status: 'pendente',
+  });
+
+  return result;
+};
+
 module.exports = {
   getTasks,
+  addTask,
 };
